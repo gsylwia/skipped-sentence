65690db6dd993fe302cb7a6f648407f696b57049
Sylwia <g.sylwia@vp.pl>
2022-10-03 01:04:58 +0200

    change for loop to forEach add validation

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
+let sentence = "ALa ma kota";
+let skip = 2;
+
 const getSkippedSentence = (sentence, skip) => {
 
-    if (typeof(sentence) == 'string' &&  typeof(skip) == 'number') {
+ /*    if (typeof(sentence) == 'string' &&  typeof(skip) == 'number') {
         console.log("OK")
     } else {
         console.log("Not OK");
-    }
-
-    for (let i=0; i<sentence.length; i+= skip) {
+    } */
+    let newSentence = [];
+    let arrSentence = sentence.split('');
+    
+    arrSentence.forEach(function(e) {
+        newSentence.push(e);
+    });
+		
+    console.log(newSentence);
+    
+/*    	let skippedLetter = newSentence.filter( function(letter) {
+   	        let word = letter[0] += skip;
+   	        console.log(word);
+   	    });
+   	    
+   	    console.log(skippedLetter); */
+    
+    
+		let newString = newSentence.join("");
+		console.log(newString);
+  /*   for (let i=0; i<sentence.length; i+= skip) {
         let letter = console.log(sentence[i]);    
-    }
+    } */
+ 		
 }; 
 
-getSkippedSentence("Ala ma kota" , 2);
+getSkippedSentence(sentence, skip);
