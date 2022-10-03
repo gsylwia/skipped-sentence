972c4a5205cdd40d303e6821119abce797297ad5
Sylwia <g.sylwia@vp.pl>
2022-10-03 23:20:24 +0200

    use filter method

--- script.js
+++ script.js
@@ -3,34 +3,14 @@ let skip = 2;
 
 const getSkippedSentence = (sentence, skip) => {
 
- /*    if (typeof(sentence) == 'string' &&  typeof(skip) == 'number') {
-        console.log("OK")
-    } else {
-        console.log("Not OK");
-    } */
-    let newSentence = [];
-    let arrSentence = sentence.split('');
-    
-    arrSentence.forEach(function(e) {
-        newSentence.push(e);
-    });
-		
-    console.log(newSentence);
-    
-/*    	let skippedLetter = newSentence.filter( function(letter) {
-   	        let word = letter[0] += skip;
-   	        console.log(word);
-   	    });
-   	    
-   	    console.log(skippedLetter); */
-    
+    if (typeof(sentence) == 'string' &&  typeof(skip) == 'number') {
+        const newSentence = sentence.split("").filter((el, index) => index % skip === 0);
     
 		let newString = newSentence.join("");
 		console.log(newString);
-  /*   for (let i=0; i<sentence.length; i+= skip) {
-        let letter = console.log(sentence[i]);    
-    } */
- 		
+    } 
+    
+    return [];		
 }; 
 
 getSkippedSentence(sentence, skip);
